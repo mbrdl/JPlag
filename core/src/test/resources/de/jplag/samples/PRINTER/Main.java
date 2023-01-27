b2487bdd705f633c88c9d46952bea80bda36c4c8
Moritz Brödel <moritz.broedel@gmail.com>
2023-01-27 10:33:57 +0100

    Add printer samples

---
+++ core/src/test/resources/de/jplag/samples/PRINTER/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+
+    public static void main(String[] args) {
+		var hellos = new String[]{"hellos"};
+		int i = 0;
+		while (i < hellos.length) {
+		    System.out.println("Hello " + hellos[i]);
+	    	i++;
+		}
+    }
+}
---
+++ core/src/test/resources/de/jplag/samples/PRINTER/Other.java
@@ -0,0 +1,12 @@
+public class Other {
+
+    public static void main(String[] args) {
+        int i = 0;
+        var hellos = new String[]{"hellos"};
+        while (i < hellos.length) {
+            System.out.println("Hello " + hellos[i]);
+            i++;
+            boolean done = false;
+        }
+    }
+}
